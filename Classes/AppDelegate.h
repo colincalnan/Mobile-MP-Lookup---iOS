cff0712c231ee87b4f08267c1aca4e5c0df5931c
Colin <colin@raisedeyebrow.com>
2011-09-12 14:55:58 -0700

    First commit

---
+++ Classes/AppDelegate.h
@@ -0,0 +1,29 @@
+//
+//  AppDelegate.h
+//  FirstApp
+//
+//  Created by Colin Calnan on 11-09-09.
+//  Copyright Raised Eyebrow Web Studio Inc. 2011. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#ifdef PHONEGAP_FRAMEWORK
+	#import <PhoneGap/PhoneGapDelegate.h>
+#else
+	#import "PhoneGapDelegate.h"
+#endif
+
+@interface AppDelegate : PhoneGapDelegate {
+
+	NSString* invokeString;
+}
+
+// invoke string is passed to your app on launch, this is only valid if you 
+// edit FirstApp.plist to add a protocol
+// a simple tutorial can be found here : 
+// http://iphonedevelopertips.com/cocoa/launching-your-own-application-via-a-custom-url-scheme.html
+
+@property (copy)  NSString* invokeString;
+
+@end
+
---
+++ main.m
@@ -0,0 +1,17 @@
+//
+//  main.m
+//  FirstApp
+//
+//  Created by Colin Calnan on 11-09-09.
+//  Copyright Raised Eyebrow Web Studio Inc. 2011. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+int main(int argc, char *argv[]) {
+    
+    NSAutoreleasePool * pool = [[NSAutoreleasePool alloc] init];
+    int retVal = UIApplicationMain(argc, argv, nil, @"AppDelegate");
+    [pool release];
+    return retVal;
+}
